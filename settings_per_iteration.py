c7a4406d850732a985d4c52772cbc4456a350685
Brianlih <brianlih410@gmail.com>
2023-02-22 14:45:49 +0800

    Set up Random environment

--- settings_per_iteration.py
+++ settings_per_iteration.py
@@ -15,7 +15,7 @@ def init(number_of_requests, number_of_VNF_types):
         number_of_needed_VNF_by_request_i = random.randint(lower_bound_of_F_i, upper_bound_of_F_i)
         tmp = random.sample(settings.F, number_of_needed_VNF_by_request_i)
         F_i.append(tmp)
-    print("F_i = ", F_i)
+    # print("F_i = ", F_i)
 
     psi_f = []
     count = 0
@@ -27,7 +27,7 @@ def init(number_of_requests, number_of_VNF_types):
                     break
         psi_f.append(count / number_of_requests)
         count = 0
-    print("psi_f = ", psi_f)
+    # print("psi_f = ", psi_f)
 
     profit_i = []
     for i in range(number_of_requests):
@@ -35,18 +35,18 @@ def init(number_of_requests, number_of_VNF_types):
         for j in range(len(F_i[i])):
             profit += settings.eta_f[F_i[i][j]] * (1 + psi_f[F_i[i][j]]) * settings.cpu_f[F_i[i][j]]
         profit_i.append(profit)
-    print("profit_i = ", profit_i)
+    # print("profit_i = ", profit_i)
 
     r_i = []
     for i in range(number_of_requests):
         r = random.randint(lower_bound_of_r_i, upper_bound_of_r_i)
         r_i.append(r * len(F_i[i]))
-    print("r_i = ", r_i)
+    # print("r_i = ", r_i)
 
     s_i = []
     for i in range(number_of_requests):
         s_i.append(random.randint(0, pre_settings.number_of_nodes - 1))
-    print("s_i = ", s_i)
+    # print("s_i = ", s_i)
 
     e_i = []
     buffer = random.randint(0, pre_settings.number_of_nodes - 1)
@@ -54,5 +54,5 @@ def init(number_of_requests, number_of_VNF_types):
         while(buffer == s_i[i]):
             buffer = random.randint(0, pre_settings.number_of_nodes - 1)
         e_i.append(buffer)
-    print("e_i = ", e_i)
-    print("==================================================================================")
+    # print("e_i = ", e_i)
+    # print("==================================================================================")
