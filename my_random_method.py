9b7247320e8814aa072ff4704c1c88d8f407348d
Brianlih <brianlih410@gmail.com>
2023-02-22 17:05:31 +0800

    Improve Random method: 1. Random select which reqeust to place first. 2. Sort all nodes by node value. 3. Place vnf at the node from the highest node value to the lowest one.

--- my_random_method.py
+++ my_random_method.py
@@ -5,8 +5,6 @@ import random
 import settings
 
 def check_if_meet_delay_requirement(request_assign_node, i, data):
-    if -2 in request_assign_node[i]:
-        return False
     tau_vnf_i = 0
     tau_i = 0
     first_vnf = -1
@@ -29,6 +27,47 @@ def check_if_meet_delay_requirement(request_assign_node, i, data):
         return True
     return False
 
+def calculate_two_phase_length_of_nodes(pre_node, r_index, data):
+    two_phases_len = []
+    for i in range(len(data.nodes)):
+        length = settings.v2v_shortest_path_length(
+                data.G,
+                pre_node,
+                i)
+        length += settings.v2v_shortest_path_length(
+                data.G,
+                i,
+                data.e_i[r_index])
+        two_phases_len.append(length)
+    return two_phases_len
+
+def sort_nodes(rest_cpu_v, r_index, vnf_type, buffer_request_assign_node, data):
+    node_value = []
+    is_first_vnf = settings.check_is_first_vnf(vnf_type, data.F_i[r_index])
+    if is_first_vnf:
+        pre_node = data.s_i[r_index]
+    else:
+        pre_vnf_index = data.F_i[r_index].index(vnf_type) - 1
+        pre_vnf = data.F_i[r_index][pre_vnf_index]
+        pre_node = buffer_request_assign_node[r_index][pre_vnf]
+    two_phases_len = calculate_two_phase_length_of_nodes(pre_node, r_index, data)
+    max_tpl = max(two_phases_len)
+    min_tpl = min(two_phases_len)
+    max_rc = max(rest_cpu_v)
+    min_rc = min(rest_cpu_v)
+    for i in range(len(data.nodes)):
+        node_value.append(
+            ((rest_cpu_v[i] - min_rc + 1)
+            / (max_rc - min_rc + 1))
+            / ((two_phases_len[i] - min_tpl + 1)
+                / (max_tpl - min_tpl + 1)))
+    sorted_nodes = sorted(
+        data.nodes,
+        key= lambda node : node_value[node],
+        reverse=True)
+    
+    return sorted_nodes
+
 def main(data_from_cplex):
     data = data_from_cplex
     start_time = time.time()
@@ -48,59 +87,52 @@ def main(data_from_cplex):
         buffer_F_i.remove(request)
         r_index = data.F_i.index(request)
 
-        # find all path
-        all_paths = nx.all_simple_paths(data.G, source=data.s_i[r_index], target=data.e_i[r_index])
-        all_paths_list = list(all_paths)
-
         # resources befor placing request
         buffer_cpu = deepcopy(rest_cpu_v)
         buffer_mem = deepcopy(rest_mem_v)
         buffer_vnf_on_node = deepcopy(vnf_on_node)
         buffer_request_assign_node = deepcopy(request_assign_node)
-        while True:
-            assigned_count = 0
-            
-            # randomly select a path
-            path = random.choice(all_paths_list)
-            all_paths_list.remove(path)
-
-            for vnf_type in range(data.number_of_VNF_types):
-                if vnf_type not in request:
-                    buffer_request_assign_node[r_index][vnf_type] = -1
-                else:
-                    for node in path:
-                        if vnf_type not in buffer_vnf_on_node[node]:
-                            if buffer_mem[node] >= 1 and data.cpu_f[vnf_type] <= buffer_cpu[node]:
-                                buffer_vnf_on_node[node].append(vnf_type)
-                                buffer_mem[node] -= 1
-                                buffer_request_assign_node[r_index][vnf_type] = node
-                                buffer_cpu[node] -= data.cpu_f[vnf_type]
-                                assigned_count += 1
-                                break
-                        else:
-                            if data.cpu_f[vnf_type] <= buffer_cpu[node]:
-                                buffer_request_assign_node[r_index][vnf_type] = node
-                                buffer_cpu[node] -= data.cpu_f[vnf_type]
-                                assigned_count += 1
-                                break
-            if (check_if_meet_delay_requirement(buffer_request_assign_node, r_index, data) and
-                assigned_count == len(request)):
+        assigned_count = 0
+        flag = False
+        for vnf_type in request:
+            sorted_nodes = sort_nodes(buffer_cpu, r_index, vnf_type, buffer_request_assign_node, data)
+            for node in sorted_nodes:
+                if vnf_type not in buffer_vnf_on_node[node]:
+                    if buffer_mem[node] >= 1 and data.cpu_f[vnf_type] <= buffer_cpu[node]:
+                        buffer_vnf_on_node[node].append(vnf_type)
+                        buffer_mem[node] -= 1
+                        buffer_request_assign_node[r_index][vnf_type] = node
+                        buffer_cpu[node] -= data.cpu_f[vnf_type]
+                        assigned_count += 1
+                        break
+                elif data.cpu_f[vnf_type] <= buffer_cpu[node]:
+                    if data.cpu_f[vnf_type] <= buffer_cpu[node]:
+                        buffer_request_assign_node[r_index][vnf_type] = node
+                        buffer_cpu[node] -= data.cpu_f[vnf_type]
+                        assigned_count += 1
+                        break
+                if node == sorted_nodes[-1]:
+                    flag = True
+            if flag:
+                # Return to the state before placing request
+                buffer_cpu = rest_cpu_v
+                buffer_mem = rest_mem_v
+                buffer_vnf_on_node = vnf_on_node
+                buffer_request_assign_node = request_assign_node
+                break
+        if not flag:
+            if check_if_meet_delay_requirement(buffer_request_assign_node, r_index, data):
                 rest_cpu_v = buffer_cpu
                 rest_mem_v = buffer_mem
                 vnf_on_node = buffer_vnf_on_node
                 request_assign_node = buffer_request_assign_node
                 buffer_z[r_index] = 1
-                break
             else:
                 # return to the state before placing request
                 buffer_cpu = rest_cpu_v
                 buffer_mem = rest_mem_v
                 buffer_vnf_on_node = vnf_on_node
                 buffer_request_assign_node = request_assign_node
-                if len(all_paths_list) <= 0:
-                    # request can not be placed on the network completely
-                    # so reject it
-                    break
 
     # print("Random solution: ", request_assign_node)
     end_time = time.time()
